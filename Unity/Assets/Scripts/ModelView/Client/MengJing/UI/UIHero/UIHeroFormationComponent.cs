5780c76cf0dafa74146e7f426246b2626b89a1af
FortuneCat0v0 <1142094319@qq.com>
2025-10-28 11:55:53 +0800

    .

--- Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIHero/UIHeroFormationComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIHero/UIHeroFormationComponent.cs
@@ -12,19 +12,11 @@ namespace ET.Client
 
         public int ShowHeroType;
         public List<UIFormationHeroItem> UIFormationHeroItemList { get; set; } = new();
+        public List<UIFormationSlotItem> UIFormationSlotItemList { get; set; } = new();
 
         public TMP_Text Text_FormationCount;
         public TMP_Text Text_TotalCP;
-        public UIFormationSlotItem UIFormationSlotItem_1 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_2 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_3 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_4 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_5 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_6 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_7 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_8 { get; set; }
-        public UIFormationSlotItem UIFormationSlotItem_9 { get; set; }
-        
+        public Transform Transform_UIFormationSlotItemList;
         public Button Button_Type_All;
         public Button Button_Type_Melee;
         public Button Button_Type_Ranged;
