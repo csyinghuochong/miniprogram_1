9cb5268f584f5bb55e384f66df270f87bec5242a
FortuneCat0v0 <1142094319@qq.com>
2025-11-05 15:57:46 +0800

    ...

---
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Buff/Action/Buff_Attribute.cs
@@ -0,0 +1,25 @@
+﻿namespace ET.Client
+{
+    public class Buff_Attribute : BuffCHandler
+    {
+        public override void OnInit(BuffC buffc)
+        {
+        }
+
+        public override void OnReset(BuffC buffc, float endTime)
+        {
+        }
+
+        public override void OnExecute(BuffC buffc)
+        {
+        }
+
+        public override void OnUpdate(BuffC buffc)
+        {
+        }
+
+        public override void OnFinished(BuffC buffc)
+        {
+        }
+    }
+}
