ea4bb8573c25cded910d3f0a15aba94a812a4ade
FortuneCat0v0 <1142094319@qq.com>
2025-11-15 17:08:17 +0800

    部署在云服务器上成功 ヾ(≧▽≦*)o

--- Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
+++ Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
@@ -3,7 +3,7 @@ namespace ET
     public static partial class ConstValue
     {
         public const string RouterHttpHostInter =  "127.0.0.1";
-        public const string RouterHttpHostOuter =  "47.94.107.92";
+        public const string RouterHttpHostOuter =  "115.190.237.40";
         public const int RouterHttpPort = 30300;
         public const int SessionTimeoutTime = 60 * 1000;
     }
