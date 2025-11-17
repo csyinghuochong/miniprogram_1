882c88d7255c6bd5da1aac1a26b09802abfc0168
FortuneCat0v0 <1142094319@qq.com>
2025-11-17 17:46:42 +0800

    .

---
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/UIMain/UIMainSkillItemSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ET.Client
+{
+    [EntitySystemOf(typeof(UIMainSkillItem))]
+    [FriendOf(typeof(UIMainSkillItem))]
+    public static partial class UIMainSkillItemSystem
+    {
+        [EntitySystem]
+        private static void Awake(this UIMainSkillItem self, GameObject gameObject)
+        {
+            self.GameObject = gameObject;
+        }
+
+        [EntitySystem]
+        private static void Destroy(this UIMainSkillItem self)
+        {
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIMain/UIMainSkillItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ET.Client
+{
+    [ComponentOf(typeof(UIMainSkillComponent))]
+    public class UIMainSkillItem : Entity, IAwake<GameObject>, IDestroy
+    {
+        public GameObject GameObject { get; set; }
+    }
+}
