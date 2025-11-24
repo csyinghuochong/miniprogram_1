6d40ab4d4c0dac754cf0af8edba80d0d685be679
FortuneCat0v0 <1142094319@qq.com>
2025-11-24 12:02:43 +0800

    Skill_洗礼

---
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Skill/Action/Skill_洗礼.cs
@@ -0,0 +1,6 @@
+﻿namespace ET.Client
+{
+    public class Skill_洗礼 : Skill_Common
+    {
+    }
+}
---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Skill/Action/Skill_洗礼.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace ET.Server
+{
+    public class Skill_洗礼 : SkillHandlerS
+    {
+        public override void OnInit(SkillS skill)
+        {
+            skill.ICheckShape = skill.CreateCheckShape(0);
+        }
+
+        public override void OnExecute(SkillS skill)
+        {
+            bool triggerTeam = false;
+            
+            List<EntityRef<Unit>> entities = skill.TheUnitFrom.GetParent<UnitComponent>().GetAll();
+            for (int i = entities.Count - 1; i >= 0; i--)
+            {
+                Unit defendUnit = entities[i];
+
+                if (!triggerTeam && UnitHelper.IsTeam(skill.TheUnitFrom, defendUnit))
+                {
+                    foreach (int id in skill.SkillConfig.BuffID)
+                    {
+                        skill.SkillBuff(id, defendUnit);
+                    }
+
+                    triggerTeam = true;
+                }
+
+                if (!skill.TheUnitFrom.IsCanAttackUnit(defendUnit))
+                {
+                    continue;
+                }
+
+                if (!skill.ICheckShape.Contains(defendUnit.Position))
+                {
+                    continue;
+                }
+
+                Function_Fight.Fight(skill.TheUnitFrom, defendUnit, skill);
+            }
+
+            skill.SkillState = SkillState.Finished;
+        }
+
+        public override void OnUpdate(SkillS skill, float deltaTime)
+        {
+        }
+
+        public override void OnFinished(SkillS skill)
+        {
+        }
+    }
+}
