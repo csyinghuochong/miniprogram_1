b71baa160770c47b9a6a780cb8e6e2b85adcf8a0
FortuneCat0v0 <1142094319@qq.com>
2025-11-24 14:32:15 +0800

    Skill_剧变.cs

---
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Skill/Action/Skill_剧变.cs
@@ -0,0 +1,6 @@
+﻿namespace ET.Client
+{
+    public class Skill_剧变 : Skill_Common
+    {
+    }
+}
---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Skill/Action/Skill_剧变.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace ET.Server
+{
+    public class Skill_剧变 : SkillHandlerS
+    {
+        public override void OnInit(SkillS skill)
+        {
+            skill.ICheckShape = skill.CreateCheckShape(0);
+        }
+
+        public override void OnExecute(SkillS skill)
+        {
+            List<EntityRef<Unit>> entities = skill.TheUnitFrom.GetParent<UnitComponent>().GetAll();
+            for (int i = entities.Count - 1; i >= 0; i--)
+            {
+                Unit defendUnit = entities[i];
+
+                if (!skill.TheUnitFrom.IsCanAttackUnit(defendUnit))
+                {
+                    continue;
+                }
+
+                if (!skill.ICheckShape.Contains(defendUnit.Position))
+                {
+                    continue;
+                }
+
+                foreach (int id in skill.SkillConfig.BuffID)
+                {
+                    skill.SkillBuff(id, defendUnit);
+                }
+            }
+
+            skill.SkillState = SkillState.Finished;
+        }
+
+        public override void OnUpdate(SkillS skill, float deltaTime)
+        {
+        }
+
+        public override void OnFinished(SkillS skill)
+        {
+        }
+    }
+}
