6448298b15617ea392e99d1bfb2994964f61f403
FortuneCat0v0 <1142094319@qq.com>
2025-11-24 14:34:29 +0800

    .

---
+++ Unity/Assets/Scripts/Hotfix/Client/MengJing/Skill/Action/Skill_致命链接.cs
@@ -0,0 +1,6 @@
+﻿namespace ET.Client
+{
+    public class Skill_致命链接 : Skill_Common
+    {
+    }
+}
---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Skill/Action/Skill_致命链接.cs
@@ -0,0 +1,22 @@
+﻿namespace ET.Server
+{
+    public class Skill_致命链接 : SkillHandlerS
+    {
+        public override void OnInit(SkillS skill)
+        {
+        }
+
+        public override void OnExecute(SkillS skill)
+        {
+            skill.SkillState = SkillState.Finished;
+        }
+
+        public override void OnUpdate(SkillS skill, float deltaTime)
+        {
+        }
+
+        public override void OnFinished(SkillS skill)
+        {
+        }
+    }
+}
