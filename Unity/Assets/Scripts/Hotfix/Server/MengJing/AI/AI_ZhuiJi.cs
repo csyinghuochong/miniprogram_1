520db62d7671893c0765d13df61132573d9e9527
FortuneCat0v0 <1142094319@qq.com>
2025-11-25 10:57:24 +0800

    怪物被打了才还手

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/AI/AI_ZhuiJi.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/AI/AI_ZhuiJi.cs
@@ -10,6 +10,12 @@ namespace ET.Server
             Unit myUnit = aiComponent.GetParent<Unit>();
             UnitComponent unitComponent = myUnit.GetParent<UnitComponent>();
 
+            // 怪物被打了才还手
+            if (myUnit.Type == UnitType.Monster && aiComponent.BeAttackId == 0)
+            {
+                return 1;
+            }
+
             // 搜索最近的敌人
             Unit closestEnemy = null;
             float closestDistance = float.MaxValue;
