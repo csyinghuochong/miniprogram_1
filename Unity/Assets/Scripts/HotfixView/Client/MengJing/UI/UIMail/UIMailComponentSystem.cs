3cfb0824f5998e8afee716e0ff6b38398bdff59d
Loc <1638974855@qq.com>
2025-11-27 18:33:26 +0800

    no message

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/UIMail/UIMailComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/UIMail/UIMailComponentSystem.cs
@@ -17,12 +17,25 @@ namespace ET.Client
             self.Button_Close = rc.Get<GameObject>("Button_Close").GetComponent<Button>();
             self.Content_UIMailItem = rc.Get<GameObject>("Content_UIMailItem").transform;
             self.UIMailItem = rc.Get<GameObject>("UIMailItem");
+            self.UIMailItem.SetActive(false);
 
             self.Button_DeleteAll.onClick.AddListener(() => { self.OnDeleteAll(); });
             self.Button_GetAll.onClick.AddListener(() => { self.OnGetAll(); });
             self.Button_Close.onClick.AddListener(() => { self.Root().GetComponent<UIComponent>().Remove(UIType.UIMail); });
         }
 
+        [EntitySystem]
+        private static void Destroy(this UIMailComponent self)
+        {
+            self.UIMailItemList.Clear();
+            self.UIMailItem = null;
+        }
+
+        private static void UpdateMailList(this UIMailComponent self)
+        {
+            
+        }
+        
         private static void OnDeleteAll(this UIMailComponent self)
         {
             Log.Warning("删除了所有邮件");
@@ -32,7 +45,6 @@ namespace ET.Client
         {
             Log.Warning("领取了所有邮件");
         }
-        
         
     }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIMail/UIMaliComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIMail/UIMaliComponent.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace ET.Client
 {
     [ComponentOf(typeof(UI))]
-    public class UIMailComponent: Entity, IAwake
+    public class UIMailComponent: Entity, IAwake,IDestroy
     {
+        public List<UIMailItem> UIMailItemList { get; set; } = new();
+        
         public Button Button_DeleteAll;
         public Button Button_GetAll;
         public Button Button_Close;
