f63d353b72f6f7517c84fcdca34dc697176e32a5
FortuneCat0v0 <1142094319@qq.com>
2025-12-04 14:48:53 +0800

    ！！！地面和一些小花等是显示在最底下的，Sorting Layer设置成Background，通过调节Order In Layer调整花和地板的显示顺序。要面向摄像机的物体，如角色和草等Sorting Layer设置成Default、Order In Layer设置成0，他们之间的显示顺序摄像机会自动根据远近处理。如果大于Default的话图片会一直显示在特效的上面。。。

--- Unity/Assets/Scripts/Mono/FacingCamera.cs
+++ Unity/Assets/Scripts/Mono/FacingCamera.cs
@@ -6,22 +6,42 @@ namespace ET
 {
     public class FacingCamera : MonoBehaviour
     {
-        private Transform[] childs;
+        private Transform[] targetTransforms;
 
         void Start()
         {
-            this.childs = new Transform[this.transform.childCount];
-            for (int i = 0; i < this.transform.childCount; i++)
-            {
-                this.childs[i] = this.transform.GetChild(i);
-            }
+            CollectSpriteRendererTransforms(transform);
         }
 
         void Update()
         {
-            for (int i = 0; i < this.childs.Length; i++)
+            if (targetTransforms == null || targetTransforms.Length == 0) return;
+
+            for (int i = 0; i < targetTransforms.Length; i++)
             {
-                this.childs[i].rotation = Camera.main.transform.rotation;
+                targetTransforms[i].rotation = Camera.main.transform.rotation;
+            }
+        }
+
+        private void CollectSpriteRendererTransforms(Transform parentTrans)
+        {
+            List<Transform> tempList = new List<Transform>();
+
+            RecursiveCollect(parentTrans, tempList);
+
+            targetTransforms = tempList.ToArray();
+        }
+
+        private void RecursiveCollect(Transform currentTrans, List<Transform> resultList)
+        {
+            if (currentTrans.GetComponent<SpriteRenderer>() != null)
+            {
+                resultList.Add(currentTrans);
+            }
+
+            for (int i = 0; i < currentTrans.childCount; i++)
+            {
+                RecursiveCollect(currentTrans.GetChild(i), resultList);
             }
         }
     }
