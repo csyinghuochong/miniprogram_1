7b803170c57fc6f0145b32f6256c20c66e237793
FortuneCat0v0 <1142094319@qq.com>
2025-12-08 18:09:07 +0800

    SessionTimeoutTime改为5秒

--- Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
+++ Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
@@ -5,6 +5,6 @@ namespace ET
         public const string RouterHttpHostInter =  "127.0.0.1";
         public const string RouterHttpHostOuter =  "115.190.237.40";
         public const int RouterHttpPort = 30300;
-        public const int SessionTimeoutTime = 60 * 1000;
+        public const int SessionTimeoutTime = 5 * 1000;
     }
 }
