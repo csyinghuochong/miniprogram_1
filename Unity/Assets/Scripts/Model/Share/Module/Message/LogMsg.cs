825c536f1cb64186e9a5b376f82fb0280975ca2d
FortuneCat0v0 <1142094319@qq.com>
2025-12-10 12:44:02 +0800

    M2C_NoticeUnitTransformList Debug忽略

--- Unity/Assets/Scripts/Model/Share/Module/Message/LogMsg.cs
+++ Unity/Assets/Scripts/Model/Share/Module/Message/LogMsg.cs
@@ -2,16 +2,17 @@
 
 namespace ET
 {
-    public class LogMsg: Singleton<LogMsg>, ISingletonAwake
+    public class LogMsg : Singleton<LogMsg>, ISingletonAwake
     {
         private readonly HashSet<ushort> ignore = new()
         {
             OuterMessage.C2R_Ping,
             OuterMessage.R2C_Ping,
-            OuterMessage.C2G_Ping, 
-            OuterMessage.G2C_Ping, 
-            OuterMessage.C2G_Benchmark, 
+            OuterMessage.C2G_Ping,
+            OuterMessage.G2C_Ping,
+            OuterMessage.C2G_Benchmark,
             OuterMessage.G2C_Benchmark,
+            OuterMessage.M2C_NoticeUnitTransformList,
             //OuterMessage.C2M_PathfindingRequest,
             //OuterMessage.C2M_PathfindingResult,
             //OuterMessage.M2C_PathfindingResult,
@@ -32,7 +33,8 @@ namespace ET
             {
                 return;
             }
-            fiber.Log.Debug(msg.ToString() );
+
+            fiber.Log.Debug(msg.ToString());
         }
     }
 }
