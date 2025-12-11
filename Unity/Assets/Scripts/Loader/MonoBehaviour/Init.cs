0656ce404557d66f23b95d619fedba5092d2e3d9
FortuneCat0v0 <1142094319@qq.com>
2025-12-11 17:11:21 +0800

    更新逻辑调整

--- Unity/Assets/Scripts/Loader/MonoBehaviour/Init.cs
+++ Unity/Assets/Scripts/Loader/MonoBehaviour/Init.cs
@@ -1,135 +1,134 @@
 ﻿using System;
 using System.Collections;
 using CommandLine;
+using UniFramework.Event;
 using UnityEngine;
 using YooAsset;
 
 namespace ET
 {
-	
-	[EnableClass]
-	public class Init: MonoBehaviour
-	{
-		public bool EditorMode;
-		public int VersionMode = ET.VersionMode.Alpha;
-		private EPlayMode ePlayMode;
-		
-		private int BigVersion = 0;
-		private int BigVersionIOS = 0;
-		
-		public Action<bool> OnApplicationFocusHandler;
+    [EnableClass]
+    public class Init : MonoBehaviour
+    {
+        public bool EditorMode;
+        public int VersionMode = ET.VersionMode.Alpha;
+        private EPlayMode ePlayMode;
 
-		private void Start()
-		{
-			this.StartAsync().Coroutine();
-		}
-		
-		private async ETTask StartAsync()
-		{
-			DontDestroyOnLoad(gameObject);
-			
-			AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
-			{
-				Log.Error(e.ExceptionObject.ToString());
-			};
+        private int BigVersion = 0;
+        private int BigVersionIOS = 0;
 
-			// 命令行参数
-			string[] args = "".Split(" ");
-			Parser.Default.ParseArguments<Options>(args)
-				.WithNotParsed(error => throw new Exception($"命令行格式错误! {error}"))
-				.WithParsed((o)=>World.Instance.AddSingleton(o));
-			Options.Instance.StartConfig = $"StartConfig/Localhost";
+        public Action<bool> OnApplicationFocusHandler;
 
-			Options.Instance.Develop =  VersionMode >= ET.VersionMode.Beta ?0 : 1;
-			Options.Instance.LogLevel = VersionMode >= ET.VersionMode.Beta ?3 : 1;// 打印Debug Message消耗较大，可根据需要改为 3
-			
-			World.Instance.AddSingleton<Logger>().Log = new UnityLogger();
-			ETTask.ExceptionHandler += Log.Error;
-			
-			World.Instance.AddSingleton<TimeInfo>();
-			World.Instance.AddSingleton<FiberManager>();
-			
-			GlobalConfig globalConfig = Resources.Load<GlobalConfig>("GlobalConfig");
-			ePlayMode = globalConfig.EPlayMode;
-			
-			// YooAssets.SetDownloadSystemBreakpointResumeFileSize(5 * 1024 * 1024);
-			await World.Instance.AddSingleton<ResourcesComponent>().CreatePackageAsync("DefaultPackage",ePlayMode, true);
-			
-			// 游戏管理器
-			GameManager.Instance.Behaviour = this;
-			//OnStartGame();
-		}
-		
-		public void OnStartGame()
-		{
-			if (this.ePlayMode != EPlayMode.WebPlayMode)
-			{
-				// 开始补丁更新流程
-				TogglePatchWindow(true);
-				StartCoroutine(StartUpdate(ePlayMode));
-			}
-			else
-			{
-				// WebGL不支持下载器
-				OnUpdaterDone().Coroutine();
-			}
-		}
+        private void Start()
+        {
+            this.StartAsync().Coroutine();
+        }
 
-		IEnumerator StartUpdate(EPlayMode ePlayMode)
-		{
-			// 开始补丁更新流程
-			// PatchOperation operation = new PatchOperation("DefaultPackage", EDefaultBuildPipeline.BuiltinBuildPipeline.ToString(), ePlayMode);
-			PatchOperation operation = new PatchOperation("DefaultPackage", "2", ePlayMode);
-			operation.UpdateDownHandler = () => { OnUpdaterDone().Coroutine(); };
-			YooAssets.StartOperation(operation);
-			yield return operation;
-		}
+        private async ETTask StartAsync()
+        {
+            DontDestroyOnLoad(gameObject);
 
-		public void TogglePatchWindow(bool show)
-		{
-			GameObject.Find("Global/UI/PopUpRoot/PatchWindow").gameObject.SetActive(show);
-		}
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) => { Log.Error(e.ExceptionObject.ToString()); };
 
-		public async ETTask OnUpdaterDone()
-		{
-			CodeLoader codeLoader = World.Instance.AddSingleton<CodeLoader>();
-			await codeLoader.DownloadAsync();
+            // 命令行参数
+            string[] args = "".Split(" ");
+            Parser.Default.ParseArguments<Options>(args)
+                    .WithNotParsed(error => throw new Exception($"命令行格式错误! {error}"))
+                    .WithParsed((o) => World.Instance.AddSingleton(o));
+            Options.Instance.StartConfig = $"StartConfig/Localhost";
 
-			codeLoader.Start();
-		}
+            Options.Instance.Develop = VersionMode >= ET.VersionMode.Beta ? 0 : 1;
+            Options.Instance.LogLevel = VersionMode >= ET.VersionMode.Beta ? 3 : 1; // 打印Debug Message消耗较大，可根据需要改为 3
 
-		private void Update()
-		{
-			TimeInfo.Instance.Update();
-			FiberManager.Instance.Update();
-		}
+            World.Instance.AddSingleton<Logger>().Log = new UnityLogger();
+            ETTask.ExceptionHandler += Log.Error;
 
-		private void LateUpdate()
-		{
-			FiberManager.Instance.LateUpdate();
-		}
-		
-		private void FixedUpdate()
-		{
-			FiberManager.Instance.FixedUpdate();
-		}
+            World.Instance.AddSingleton<TimeInfo>();
+            World.Instance.AddSingleton<FiberManager>();
 
-		private void OnApplicationQuit()
-		{
-			World.Instance.Dispose();
-		}
-		
-		// 当程序获得或者是去焦点时
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			try
-			{
-				OnApplicationFocusHandler?.Invoke(hasFocus);
-			}
-			catch (System.Exception)
-			{
-				throw;
-			}
-		}
-	}
+            GlobalConfig globalConfig = Resources.Load<GlobalConfig>("GlobalConfig");
+            ePlayMode = globalConfig.EPlayMode;
+
+            // 游戏管理器
+            GameManager.Instance.Behaviour = this;
+
+            // 初始化事件系统
+            UniEvent.Initalize();
+
+            // 初始化资源系统
+            World.Instance.AddSingleton<ResourcesComponent>();
+
+            //OnStartGame();
+        }
+
+        public void OnStartGame()
+        {
+            // 显示更新页面
+            TogglePatchWindow(true);
+
+            // 开始补丁更新流程
+            StartCoroutine(StartUpdate());
+        }
+
+        public void TogglePatchWindow(bool show)
+        {
+            GameObject.Find("Global/UI/PopUpRoot/PatchWindow").gameObject.SetActive(show);
+        }
+
+        private IEnumerator StartUpdate()
+        {
+            PatchOperation operation = new PatchOperation("DefaultPackage", ePlayMode);
+            operation.UpdateDownHandler = () => { OnUpdaterDone().Coroutine(); };
+            YooAssets.StartOperation(operation);
+            yield return operation;
+        }
+
+        // 更新完成
+        private async ETTask OnUpdaterDone()
+        {
+            // 设置默认的资源包
+            var gamePackage = YooAssets.GetPackage("DefaultPackage");
+            YooAssets.SetDefaultPackage(gamePackage);
+            
+            // 加载热更代码
+            CodeLoader codeLoader = World.Instance.AddSingleton<CodeLoader>();
+            await codeLoader.DownloadAsync();
+
+            codeLoader.Start();
+        }
+
+        private void Update()
+        {
+            TimeInfo.Instance.Update();
+            FiberManager.Instance.Update();
+        }
+
+        private void LateUpdate()
+        {
+            FiberManager.Instance.LateUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            FiberManager.Instance.FixedUpdate();
+        }
+
+        private void OnApplicationQuit()
+        {
+            World.Instance.Dispose();
+        }
+
+        // 当程序获得或者是去焦点时
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            try
+            {
+                OnApplicationFocusHandler?.Invoke(hasFocus);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
 }
---
+++ Unity/Assets/Scripts/Loader/ResourcesComponent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using YooAsset;
+
+namespace ET
+{
+    public class ResourcesComponent : Singleton<ResourcesComponent>, ISingletonAwake
+    {
+        public void Awake()
+        {
+            YooAssets.Initialize();
+
+            BetterStreamingAssets.Initialize();
+        }
+
+        protected override void Destroy()
+        {
+            YooAssets.Destroy();
+        }
+
+        /// <summary>
+        /// 主要用来加载dll config aotdll，因为这时候纤程还没创建，无法使用ResourcesLoaderComponent。
+        /// 游戏中的资源应该使用ResourcesLoaderComponent来加载
+        /// </summary>
+        public T LoadAssetSync<T>(string location) where T : UnityEngine.Object
+        {
+            AssetHandle handle = YooAssets.LoadAssetSync<T>(location);
+            T t = (T)handle.AssetObject;
+            handle.Release();
+            return t;
+        }
+
+        /// <summary>
+        /// 主要用来加载dll config aotdll，因为这时候纤程还没创建，无法使用ResourcesLoaderComponent。
+        /// 游戏中的资源应该使用ResourcesLoaderComponent来加载
+        /// </summary>
+        public async ETTask<T> LoadAssetAsync<T>(string location) where T : UnityEngine.Object
+        {
+            AssetHandle handle = YooAssets.LoadAssetAsync<T>(location);
+            await handle.Task;
+            T t = (T)handle.AssetObject;
+            handle.Release();
+            return t;
+        }
+
+        /// <summary>
+        /// 主要用来加载dll config aotdll，因为这时候纤程还没创建，无法使用ResourcesLoaderComponent。
+        /// 游戏中的资源应该使用ResourcesLoaderComponent来加载
+        /// </summary>
+        public async ETTask<Dictionary<string, T>> LoadAllAssetsAsync<T>(string location) where T : UnityEngine.Object
+        {
+            AllAssetsHandle allAssetsOperationHandle = YooAssets.LoadAllAssetsAsync<T>(location);
+            await allAssetsOperationHandle.Task;
+            Dictionary<string, T> dictionary = new Dictionary<string, T>();
+            foreach (UnityEngine.Object assetObj in allAssetsOperationHandle.AllAssetObjects)
+            {
+                T t = assetObj as T;
+                dictionary.Add(t.name, t);
+            }
+
+            allAssetsOperationHandle.Release();
+            return dictionary;
+        }
+    }
+}
