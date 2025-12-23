8113dfe34edc636fbc987430dad524bcbd212e60
Loc <1638974855@qq.com>
2025-12-23 16:21:42 +0800

    no message

--- Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIChat/UIChatComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIChat/UIChatComponent.cs
@@ -13,8 +13,17 @@ namespace ET.Client
         
         public Button Button_Close;
         public TMP_Text Text_Title;
+        
+        public GameObject Scroll_PublicChatItem;
         public Transform Content_UIPublicChatItem;
         public GameObject UIPublicChatItem;
+        public GameObject Scroll_PrivateChatPeopleItem;
+        public Transform Content_UIPrivateChatPeopleItem;
+        public GameObject UIPrivateChatPeopleItem;
+        public GameObject Scroll_PrivateChatItem;
+        public Transform Content_UIPrivateChatItem;
+        public GameObject UIPrivateChatItem;
+        
         public TMP_InputField InputField_Content;
         public Button Button_Emoji;
         public Button Button_Send;
