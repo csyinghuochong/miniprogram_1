c2f8462864e3a0fa189c16ae12065a8f876ac764
FortuneCat0v0 <1142094319@qq.com>
2026-01-06 11:44:39 +0800

    .

--- Unity/Assets/Scripts/Hotfix/Server/MengJing/Level/DropHelper.cs
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Level/DropHelper.cs
@@ -12,7 +12,8 @@ namespace ET.Server
             foreach (int dropId in monsterConfig.DropId)
             {
                 RewardItem rewardItem = DropHelper.DropItem(dropId);
-                float3 position = new float3(unit.Position.x + RandomHelper.RandomNumberFloat(-2, 2), unit.Position.y + RandomHelper.RandomNumberFloat(-2, 2), unit.Position.z);
+                float3 position = new float3(unit.Position.x + RandomHelper.RandomNumberFloat(-2, 2),
+                    unit.Position.y + RandomHelper.RandomNumberFloat(-2, 2), unit.Position.z);
                 UnitFactory.CreateDropItem(unit.Scene(), rewardItem.ItemId, rewardItem.ItemNum, position);
             }
         }
@@ -39,6 +40,48 @@ namespace ET.Server
             foreach (DropItemInfo dropItemInfo in allDrop)
             {
                 index += dropItemInfo.Weight;
+                if (random <= index)
+                {
+                    current = dropItemInfo;
+                    break;
+                }
+            }
+
+            return new RewardItem() { ItemId = current.ItemId, ItemNum = RandomHelper.RandomNumber(current.MinNum, current.MaxNum + 1) };
+        }
+
+        public static RewardItem DropItem_2(int dropId, int itemId, int addWeight)
+        {
+            using ListComponent<DropItemInfo> allDrop = ListComponent<DropItemInfo>.Create();
+            int totalWeight = 0;
+            DropConfig dropConfig = DropConfigCategory.Instance.Get(dropId);
+            foreach (DropItemInfo dropItemInfo in dropConfig.DropItemInfos)
+            {
+                totalWeight += dropItemInfo.Weight;
+                if (dropItemInfo.ItemId == itemId)
+                {
+                    totalWeight += addWeight;
+                }
+
+                allDrop.Add(dropItemInfo);
+            }
+
+            if (allDrop.Count == 0)
+            {
+                return new RewardItem() { ItemId = 1, ItemNum = 1 };
+            }
+
+            int random = RandomHelper.RandomNumber(1, totalWeight + 1);
+            int index = 0;
+            DropItemInfo current = allDrop[0];
+            foreach (DropItemInfo dropItemInfo in allDrop)
+            {
+                index += dropItemInfo.Weight;
+                if (dropItemInfo.ItemId == itemId)
+                {
+                    index += addWeight;
+                }
+
                 if (random <= index)
                 {
                     current = dropItemInfo;
