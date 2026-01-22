533395d214410a5b5c6682967da6c42e6b00aa55
FortuneCat0v0 <1142094319@qq.com>
2026-01-22 19:03:57 +0800

    .

---
+++ Unity/Assets/Scripts/Hotfix/Server/MengJing/Component/BattlePassComponentSystem.cs
@@ -0,0 +1,22 @@
+﻿namespace ET.Server
+{
+    [FriendOf(typeof(BattlePassComponent))]
+    [EntitySystemOf(typeof(BattlePassComponent))]
+    public static partial class BattlePassComponentSystem
+    {
+        [EntitySystem]
+        private static void Awake(this BattlePassComponent self)
+        {
+        }
+
+        [EntitySystem]
+        private static void Destroy(this BattlePassComponent self)
+        {
+        }
+
+        [EntitySystem]
+        private static void Deserialize(this BattlePassComponent self)
+        {
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Model/Server/MengJing/Component/BattlePassComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace ET.Server
+{
+    [ComponentOf(typeof(Unit))]
+    public class BattlePassComponent : Entity, IAwake, IDestroy, ITransfer, IUnitCache, IDeserialize
+    {
+    }
+}
