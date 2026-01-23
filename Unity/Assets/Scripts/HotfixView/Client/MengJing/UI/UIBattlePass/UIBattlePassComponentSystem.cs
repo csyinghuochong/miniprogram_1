46aba3fe5eca0e6ca8a11ef2bbf833277cf58744
FortuneCat0v0 <1142094319@qq.com>
2026-01-23 15:26:15 +0800

    .

--- Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/UIBattlePass/UIBattlePassComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/MengJing/UI/UIBattlePass/UIBattlePassComponentSystem.cs
@@ -5,37 +5,6 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
-    [Event(SceneType.Demo)]
-    public class DataUpdate_UpdateUserData_UIBattlePassRefresh : AEvent<Scene, UpdateUserData>
-    {
-        protected override async ETTask Run(Scene scene, UpdateUserData args)
-        {
-            if (args.UserDataType != UserDataType.Gold && args.UserDataType != UserDataType.Diamond)
-            {
-                return;
-            }
-
-            UI ui = scene.GetComponent<UIComponent>().Get(UIType.UIBattlePass);
-            if (ui == null)
-            {
-                return;
-            }
-
-            UIBattlePassComponent uiBattlePassComponent = ui.GetComponent<UIBattlePassComponent>();
-            if (args.UserDataType == UserDataType.Gold)
-            {
-                uiBattlePassComponent.UpdateGold();
-            }
-
-            if (args.UserDataType == UserDataType.Diamond)
-            {
-                uiBattlePassComponent.UpdateDiamond();
-            }
-
-            await ETTask.CompletedTask;
-        }
-    }
-    
     [EntitySystemOf(typeof(UIBattlePassComponent))]
     [FriendOf(typeof(UIBattlePassComponent))]
     public static partial class UIBattlePassComponentSystem
@@ -46,20 +15,13 @@ namespace ET.Client
             ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
 
             self.Button_Close = rc.Get<GameObject>("Button_Close").GetComponent<Button>();
-            self.Text_Type_Gold = rc.Get<GameObject>("Text_Type_Gold").GetComponent<TMP_Text>();
-            self.Button_AddGold = rc.Get<GameObject>("Button_AddGold").GetComponent<Button>();
-            self.Text_Type_Diamond = rc.Get<GameObject>("Text_Type_Diamond").GetComponent<TMP_Text>();
-            self.Button_AddDiamond = rc.Get<GameObject>("Button_AddDiamond").GetComponent<Button>();
             self.Content_UIBattlePassItem = rc.Get<GameObject>("Content_UIBattlePassItem").transform;
             self.UIBattlePassItem = rc.Get<GameObject>("UIBattlePassItem");
             self.UIBattlePassItem.gameObject.SetActive(false);
             self.Button_GetAllReward = rc.Get<GameObject>("Button_GetAllReward").GetComponent<Button>();
 
+            self.AddComponent<UICommonHuoBiSetComponent, GameObject>(rc.Get<GameObject>("UICommonHuoBiSet"));
             self.Button_Close.AddListener(() => { self.Root().GetComponent<UIComponent>().Remove(UIType.UIBattlePass); });
-            self.Button_AddDiamond.AddListener(() => { self.Root().GetComponent<UIComponent>().Create(UIType.UIRecharge).Coroutine(); });
-            
-            self.UpdateGold();
-            self.UpdateDiamond();
         }
 
         [EntitySystem]
@@ -68,32 +30,5 @@ namespace ET.Client
             self.UIBattlePassItemList.Clear();
             self.UIBattlePassItem = null;
         }
-        
-        public static void UpdateGold(this UIBattlePassComponent self)
-        {
-            UserInfoComponentC userInfoComponent = self.Root().GetComponent<UserInfoComponentC>();
-
-            if (userInfoComponent.Gold >= 10000)
-            {
-                self.Text_Type_Gold.SetTextFormat("{0}k", userInfoComponent.Gold / 1000);
-                return;
-            }
-
-            self.Text_Type_Gold.SetText(userInfoComponent.Gold);
-        }
-
-        public static void UpdateDiamond(this UIBattlePassComponent self)
-        {
-            UserInfoComponentC userInfoComponent = self.Root().GetComponent<UserInfoComponentC>();
-            
-            if (userInfoComponent.Diamond >= 10000)
-            {
-                self.Text_Type_Diamond.SetTextFormat("{0}k", userInfoComponent.Diamond / 1000);
-                return;
-            }
-
-            self.Text_Type_Diamond.SetText(userInfoComponent.Diamond);
-        }
-
     }
 }
--- Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIBattlePass/UIBattlePassComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/MengJing/UI/UIBattlePass/UIBattlePassComponent.cs
@@ -12,10 +12,6 @@ namespace ET.Client
         public List<UIBattlePassItem> UIBattlePassItemList { get; set; } = new();
 
         public Button Button_Close;
-        public TMP_Text Text_Type_Gold;
-        public Button Button_AddGold;
-        public TMP_Text Text_Type_Diamond;
-        public Button Button_AddDiamond;
         public Transform Content_UIBattlePassItem;
         public GameObject UIBattlePassItem;
         public Button Button_GetAllReward;
